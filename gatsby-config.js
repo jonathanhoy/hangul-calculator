574dca09a12588daf12facb50746cfe15e594e88
jonathanhoy <jonathanhoy5@gmail.com>
2020-06-13 13:30:28 -0400

    updated files, added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: ``,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Learn both Sino-Korean and Pure Korean numbers in simple mathematic exercises.`,
     author: `@jonathanhoy`,
   },
   plugins: [
@@ -24,6 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
+        icon: `src/images/icon.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
