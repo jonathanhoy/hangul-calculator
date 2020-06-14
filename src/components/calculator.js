4641aad68898be9b9bacf5e6af42bb51c7c673ed
jonathanhoy <jonathanhoy5@gmail.com>
2020-06-14 12:52:11 -0400

    updated copy to reflect no more multiplication

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -131,9 +131,9 @@ class CalculatorComponent extends React.Component {
         <p>The purpose of this app is to help with memorizing Korean numbers in both the Sino and Pure/Native number systems.</p>
         <p>Understanding that each system is used in different situations, the app's objective is purely to practice memorization.</p>
         <h3>Features</h3>
-        <p>By keeping the 'Simple numbers' setting checked, you will be limited to addition problems in the range of 1-10. Unchecking 'Simple numbers' will allow addition and multiplication problems in the range of 1-100.</p>
+        <p>By keeping the 'Simple numbers' setting checked, you will be limited to problems in the range of 1-10. Unchecking 'Simple numbers' will allow problems in the range of 1-100.</p>
         <p>The 'Multiple choice' setting changes the answer format to provide a different challenge or if you do not have a Korean keyboard.</p>
-        <p>You can hover over (desktop) or tap on (mobile devices) the Sino and Pure legends for a reference of each number system.</p>
+        <p>You can click/tap the Sino and Pure legends for a reference of each number system.</p>
         <p>Good luck in your studies!</p>
         <p>&copy; Jonathan {currentyear}</p>
       </div>
